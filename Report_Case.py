c6e2247879a070858b724ab33ed0bbb2aaa4800a
Joe Attwood <56833060+Jattwood90@users.noreply.github.com>
2020-07-29 14:44:32 +0100

    Update Report_Case.py
    Script to import a text file, and export the report case strings as a csv

--- Report_Case.py
+++ Report_Case.py
@@ -1,5 +1,16 @@
-print("Insert the name you want to turn to report case: ", end="")
-name = input(str())
+import pandas as pd
+
+def openCommand():
+    print("Copy and paste the path to the txt file you wish to use: ", end="")
+    return str(input())
+
+def openTextFile(pathfile):
+    with open(pathfile, 'r') as textfile:
+        reader = textfile.readlines()
+        out = []
+        for items in list(reader):
+            out.append(items.strip())
+        return out
 
 def reportCase(name):
     name = name.title()
@@ -10,4 +21,29 @@ def reportCase(name):
     name.append(last)
     return ' '.join(name)
 
-print(reportCase(name))
+
+def runStuff(out):
+    titleCaseList = []
+    for elements in out:
+        titleCaseList.append(reportCase(elements))
+    return titleCaseList
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            fileInput = openCommand()
+            files = openTextFile(fileInput)
+            df = pd.DataFrame(runStuff(files)).to_csv('names.csv', index=None)
+            print("Script finished. Check the folder/directory where this script is saved for 'name.csv' file.")
+        except:
+            Exception: FileNotFoundError
+            print('The file path you entered is wrong, or does not exist')
+        finally:
+            print('Do you wish to try again? (Note name.csv will be overwritten if script is run again)\n'
+                  'Press "Y" to rerun script. Press any other key to exit')
+            userInput = str(input())
+            if userInput == 'Y' or 'y':
+                continue
+            else:
+                break
