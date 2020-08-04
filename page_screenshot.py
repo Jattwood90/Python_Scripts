2b930d88ec32f6066b4f6461cd00cbad3a841ff0
Joe Attwood <56833060+Jattwood90@users.noreply.github.com>
2020-08-04 22:30:49 +0100

    Update page_screenshot.py

--- page_screenshot.py
+++ page_screenshot.py
@@ -1,33 +1,32 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 import csv
 import time
 
-png = '.png'
-
 def selenium_out():
     #opens Chrome, maximises screen and saves file in current directory of script
-    browser = webdriver.Chrome(ChromeDriverManager().install()) #this overwrites the need to update chromedriver
+    browser = webdriver.Chrome()
     browser.get(url)
     browser.maximize_window()
     time.sleep(3)
-    file = str(url[8:-1]+png)
+    file = str(url[8:]+'.png')
 
     browser.save_screenshot(file)
     browser.close()
 
 
-with open('path to csv file') as csvfile:
+with open('file.csv') as csvfile:
     #opens csv file, where list of URLs exist
     reader=csv.reader(csvfile)
 
     for i in csvfile:
-        url = i.strip()
-        #strip required to delete the '\n' that randomly appears at end of variable
-        selenium_out()
+        try:
+            url = i.strip()
+            #strip required to delete the '\n' that randomly appears at end of variable
+            selenium_out()
+        except Exception as e:
+            print(f"There was an error with {i}")
+            #very basic exception handling
 
 
 csvfile.close()
 print("Script finished!")
-
-
